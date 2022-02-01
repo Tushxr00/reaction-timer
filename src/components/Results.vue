6a69b8cf23a098066e786f7274ccdd4351af5122
Tushar <tushar.nandy@vidyamantra.com>
2022-02-01 09:34:09 +0530

    edited

---
+++ src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+  <p>Reaction time --> {{ score }} ms</p>
+  <p class="rank"> {{ rank }}</p>
+  
+</template>
+
+<script>
+export default {
+    props:['score'],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250){
+            this.rank = "Ninja Fingers"
+        }
+        else if (this.score < 400){
+            this.rank = "Rapid reflexes"
+        }
+        else{
+            this.rank = "Snail pace ... "
+        }
+    },
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: gray;
+        font-weight: bold;
+    }
+</style>
